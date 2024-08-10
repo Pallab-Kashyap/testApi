f21128c339d5171daf7205043b6d7ef8a75d122b
Pallab Kashyap <141304625+Pallab-Kashyap@users.noreply.github.com>
2024-08-10 22:37:30 +0530

    Update user.js

--- dbQuery/user.js
+++ dbQuery/user.js
@@ -1,7 +1,7 @@
 // const addUserQuery = "INSERT INTO users (last_login, username, first_name, last_name, email, date_joined) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *"
 const addUserQuery = "INSERT INTO users (username) VALUES ($1) RETURNING *"
 const findUserQuery = "SELECT * FROM users WHERE username = $1"
-const userTokenQuery = "INSERT INTO usertoken (value, username, time_created, time_expired, device_name) VALUES ($1, $2, $3, $4, $5)"
+const userTokenQuery = "INSERT INTO usertoken (value, username, time_created, time_expired, device_id) VALUES ($1, $2, $3, $4, $5)"
 const deviceQuery = "INSERT INTO device (id, os_type, register_time, username) VALUES ($1, $2, $3, $4)"
 const remoteUserTokenQuery = "INSERT INTO remoteusertoken (value, time_expired, username) VALUES ($1, $2, $3)"
 const userSyncInfoQuery = "INSERT INTO usersyncinfo (id, last_library_sync_time, username) VALUES ($1, $2, $3)"
